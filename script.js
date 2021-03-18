e39358512935710660d3b29d79fe27660ccc5c1d
Philip-spb <philip.spb@gmail.com>
2021-03-19 00:00:24 +0300

    Первый коммит

---
+++ script.js
@@ -0,0 +1,23 @@
+"use strict";
+
+// Простое общение с пользователем
+
+// const answer = prompt("Как вас зовут?");
+// alert(answer);
+// console.log("Ваш ответ: " + answer + ", тип переменной: " + typeof(answer));
+
+// "OK" или "Отмена"
+// const result = confirm('Ты тут?'); 
+
+// Возможно ввести свой ответ в поле
+// const answer = prompt("Сколько вам лет?", "18");
+
+const answers = [];
+
+// answers[0] = prompt("Как вас зовут?", "");
+// answers[1] = prompt("Какая у вас фамилия?", "");
+// answers[2] = prompt("Сколько вам лет?", "");
+
+// document.write(answers);
+
+console.log(typeof(answers));

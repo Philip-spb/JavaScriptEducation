57c58a7a6520490bb7bef3b56861d0fe86bbe9d7
Philip-spb <philip.spb@gmail.com>
2021-03-20 00:10:30 +0300

    Lesson 21 – Arrays and pseudo arrays

---
+++ lesson21.js
@@ -0,0 +1,42 @@
+// Массивы и псевдомассивы
+
+'use strict';
+
+// const arr = [1, 2, 3, 6, 8];
+
+// // arr.pop();                          // Удаляем последний элемент из массива
+// arr.push(10);                          // Добавляем элемент в конец масиива
+
+// // console.log(arr);
+
+// for (let n in arr) {
+//     console.log(arr[n]);
+// }
+
+// for (let value of arr) {
+//     console.log(value);
+// }
+
+// arr.forEach(function (item, id, arr) {   // Перебираем все элементы массива
+//     console.log(`${id}: ${item} внутри масиива ${arr}`);
+// });
+
+
+// Разбиваем строку на массив
+const str = prompt("", "");
+const products = str.split(", ");
+console.log(products);
+
+// Сортировка массива (всегда сортирует как строки, 
+// но мы можем указать callback функцию которая укажет как правильно сортировать)
+products.sort();
+
+// Объединяем массив в строку
+console.log(products.join('; '));
+
+
+const numberArr = [2, 13, 26, 8, 10];
+
+function compareNum(a, b) {
+    return a - b;
+}

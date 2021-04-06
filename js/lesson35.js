77c7d20901c5404536dcbe030874fbab21fa806f
Philip-spb <philip.spb@gmail.com>
2021-04-06 15:55:55 +0300

    Lesson 35 - Async and defer

---
+++ js/lesson35.js
@@ -0,0 +1,16 @@
+// Async and defer
+
+'use strict';
+
+const p = document.querySelectorAll('p');
+console.log(p);
+
+function loadScript(src) {
+    const script = document.createElement('script');
+    script.src = src;
+    script.async = false;
+    document.body.append(script);
+}
+
+loadScript("js/test.js");
+loadScript("js/some.js");

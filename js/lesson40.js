ff4eeb1228456408a1aa937a2191f9e57f34c37c
Philip-spb <philip.spb@gmail.com>
2021-04-08 17:14:32 +0300

    Lesson 40 - Work with date and time

---
+++ js/lesson40.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const now = new Date();
+
+// console.log(now.setDate(18));
+
+let start = new Date();
+
+for (let i = 0; i < 1000000000; i++) {
+    let some = i ** 3;
+}
+
+let end = new Date();
+
+alert(`Цикл отработал за ${end - start} миллисекунд`);

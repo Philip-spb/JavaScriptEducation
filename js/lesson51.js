3358c4bf672f0da7ee2b171f85244f459f92e25c
Philip-spb <philip.spb@gmail.com>
2021-04-13 23:06:08 +0300

    Lesson 51 – JSON

---
+++ js/lesson51.js
@@ -0,0 +1,22 @@
+// JSON формат передачи данных
+
+'use strict';
+
+const person = {
+    name: 'Alex',
+    tel: '+72332233231',
+    parents: {
+        mom: 'Olga',
+        dad: 'Mike'
+    }
+};
+
+// console.log(JSON.stringify(person));
+
+// console.log(JSON.parse(JSON.stringify(person)));
+
+const clone = JSON.parse(JSON.stringify(person));
+
+clone.parents.mom = 'Ann';
+
+console.log(clone);

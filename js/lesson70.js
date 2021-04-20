19536cf9d82b3ab068350e117aa08e3efa25557d
Philip-spb <philip.spb@gmail.com>
2021-04-20 12:36:55 +0300

    Lesson 70 – modules

---
+++ js/lesson70.js
@@ -0,0 +1,22 @@
+'use strict';
+
+const number = 1;
+
+(function () {
+    let number = 2;
+    console.log(number);
+    console.log(number + 3);
+}());
+
+console.log(number);
+
+const user = (function () {
+    const privat = function () {
+        console.log('Приватная функция');
+    };
+    return {
+        sayHallo: privat
+    };
+}());
+
+user.sayHallo();
